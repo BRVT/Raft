6ef36d1d742a27d9338e2038b8a20daf2cb9ded9
Bruno <fc49498@alunos.fc.ul.pt>
2019-10-12 17:32:12 +0100

    Criacao de threads
    Threads para mandar heartbeat e o timer de cada server estao criadas

--- RaftLibrary/src/DTO/AppendRequest.java
+++ RaftLibrary/src/DTO/AppendRequest.java
@@ -3,5 +3,20 @@ package DTO;
 public class AppendRequest {
 	
 	private int leaderID;
+	private 
+//	private int leaderTerm; //nao sei se eh int
+//	private int prevLogIndex;
+//	private int prevLogTerm;
+//	private String[] entries;
+//	private int leaderCommit;
+	
+	
+	public String AppendEntriesRPC(int term, int leaderID, int prevLogIndex, int prevLogTerm,
+			String[] entries, int leaderCommit) {
+		
+		
+		
+		return "";
+	}
 	
 }
--- ServerTFD/src/server/Server.java
+++ ServerTFD/src/server/Server.java
@@ -2,6 +2,7 @@ package server;
 
 
 import java.rmi.RemoteException;
+import java.util.*;
 
 import enums.STATE;
 import server.constants.Constants;
@@ -12,16 +13,57 @@ public class Server implements IServer {
 	private static int port;
 	private static int leader = 1234;
 	private STATE state;
+	private int term;
 
+	private Timer timer;
+	private Timer heartbeat;
+	
 	/**
 	 * @param role 
 	 * 
 	 */
 	public Server(int role){
-			this.state = STATE.values()[role];
+		this.term = 0;
+		this.state = STATE.values()[role];
+		
+		//timer do server
+		timer = new Timer();
+        timer.schedule(new RemindTask(), 5*1000);
+        
+        
+        //heartbeat
+        if(state.equals(STATE.LEADER)) {
+        	//thread do hearbeat
+        	heartbeat = new Timer();
+        	heartbeat.schedule(new RemindTaskHeartBeat(),
+                           0,        //initial delay
+                           50);  //subsequent rate
+        }
 	}
 	
-
+	class RemindTaskHeartBeat extends TimerTask {
+        int numWarningBeeps = 3;
+        public void run() {
+            sendHeartBeat();
+        }
+		
+    }
+	
+    class RemindTask extends TimerTask {
+        public void run() {
+            System.out.println("Time's up!");
+            timer.cancel(); 
+            
+            increaseTerm();
+            
+            timer = new Timer();
+            timer.schedule(new RemindTask(), 5*1000);
+        }
+    }
+    
+    
+    
+    
 	@Override
 	public String request(String s, int id) throws RemoteException {
 		if(this.isLeader()) {
@@ -43,5 +85,19 @@ public class Server implements IServer {
 	public boolean isLeader() {
 		return this.state.equals(STATE.LEADER);
 
+	}
+	
+	public void increaseTerm() {
+		this.term ++;
+	}
+	
+	public double newTimer() {
+		Random r = new Random();
+		double randomValue = 5.0 + (10.0 - 5.0) * r.nextDouble();
+		return randomValue;
+	}
+	
+	public void sendHeartBeat() {
+		System.out.println("Bip");
 	}
 }
